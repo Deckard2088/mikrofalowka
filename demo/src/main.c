7160e90f110c0ac1269a919af40bce9047efea18
K4pee <kacper.skoczylas001@gmail.com>
2026-04-21 15:23:00 +0200

    we_will_see_now_v5

--- demo/src/main.c
+++ demo/src/main.c
@@ -10,49 +10,32 @@
 #include "lpc17xx_pinsel.h"
 #include "lpc17xx_gpio.h"
 #include "lpc17xx_ssp.h"
-#include "lpc17xx_adc.h"
 #include "lpc17xx_timer.h"
 
+#include "rotary.h"
 #include "led7seg.h"
 #include "rgb.h"
 
-#define CHECKPOINT_ADC_CHANNEL ADC_CHANNEL_0
-#define CHECKPOINT_ADC_PORT    0
-#define CHECKPOINT_ADC_PIN     23
-#define CHECKPOINT_ADC_FUNC    1
+static uint8_t ch7seg = '0';
 
-static uint32_t readAdcAverage(uint8_t channel);
-static uint8_t trimToTurns(uint32_t trim);
-
-static uint8_t trimToTurns(uint32_t trim)
+static void change7Seg(uint8_t rotaryDir)
 {
-    uint32_t turns = (trim * 10) / 4096;
+    if (rotaryDir != ROTARY_WAIT) {
 
-    if (turns > 9) {
-        turns = 9;
+        if (rotaryDir == ROTARY_RIGHT) {
+            ch7seg++;
+        }
+        else {
+            ch7seg--;
+        }
+
+        if (ch7seg > '9')
+            ch7seg = '0';
+        else if (ch7seg < '0')
+            ch7seg = '9';
+
+        led7seg_setChar(ch7seg, FALSE);
     }
-
-    return (uint8_t)turns;
-}
-
-static void updateCheckpointDisplayAndLed(uint32_t trim)
-{
-    uint8_t newTarget = trimToTurns(trim);
-
-    /* Potentiometer directly drives the 7-segment digit. */
-    led7seg_setChar((uint8_t)('0' + newTarget), FALSE);
-
-    /* Default state for each cycle is LED off. */
-    rgb_setLeds(0);
-
-    if (newTarget > 0) {
-        rgb_setLeds(RGB_GREEN);
-    }
-}
-
-static uint32_t readTrimFiltered(void)
-{
-    return readAdcAverage(CHECKPOINT_ADC_CHANNEL);
 }
 
 static void init_ssp(void)
@@ -80,52 +63,14 @@ static void init_ssp(void)
     SSP_Cmd(LPC_SSP1, ENABLE);
 }
 
-static uint32_t readAdcAverage(uint8_t channel)
-{
-    uint32_t sum = 0;
-    uint32_t i = 0;
-
-    for (i = 0; i < 8; i++) {
-        ADC_StartCmd(LPC_ADC,ADC_START_NOW);
-        while (!(ADC_ChannelGetStatus(LPC_ADC,channel,ADC_DATA_DONE)));
-        sum += ADC_ChannelGetData(LPC_ADC,channel);
-    }
-
-    return (sum / 8);
-}
-static void init_adc(void)
-{
-	PINSEL_CFG_Type PinCfg;
-
-    /*
-     * Init ADC pin connect
-     * AD0.0 on P0.23
-     */
-    PinCfg.Funcnum = CHECKPOINT_ADC_FUNC;
-	PinCfg.OpenDrain = 0;
-	PinCfg.Pinmode = 0;
-    PinCfg.Pinnum = CHECKPOINT_ADC_PIN;
-    PinCfg.Portnum = CHECKPOINT_ADC_PORT;
-	PINSEL_ConfigPin(&PinCfg);
-
-	/* Configuration for ADC :
-	 * 	Frequency at 0.2Mhz
-     *  ADC channel 0, no Interrupt
-	 */
-	ADC_Init(LPC_ADC, 200000);
-    ADC_IntConfig(LPC_ADC,CHECKPOINT_ADC_CHANNEL,DISABLE);
-    ADC_ChannelCmd(LPC_ADC,CHECKPOINT_ADC_CHANNEL,ENABLE);
-
-}
-
 
 int main (void) {
 
-    uint32_t trim = 0;
+    uint8_t rotaryState = 0;
 
     init_ssp();
-    init_adc();
 
+    rotary_init();
     led7seg_init();
     rgb_init();
 
@@ -133,9 +78,14 @@ int main (void) {
     rgb_setLeds(0);
 
     while (1) {
-        trim = readTrimFiltered();
 
-        updateCheckpointDisplayAndLed(trim);
+        rotaryState = rotary_read();
+        change7Seg(rotaryState);
+
+        if (ch7seg == '0')
+            rgb_setLeds(0);
+        else
+            rgb_setLeds(RGB_GREEN);
 
         Timer0_Wait(1);
     }
